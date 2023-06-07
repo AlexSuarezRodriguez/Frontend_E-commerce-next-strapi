4691dc2b67005fff078a89721beb1e7b8b01bddd
alexSuarezR <alex.suarez@integ.ro>
2023-06-06 22:56:42 -0500

    setting varibles global of scss

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 const index = () => {
   return (
     <div>
       <h2>shop</h2>
-
+      <Button primary>Primary</Button>
     </div>
   );
 };
