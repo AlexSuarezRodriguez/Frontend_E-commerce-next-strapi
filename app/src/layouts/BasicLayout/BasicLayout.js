a834fe3de6e11d26175266806e75e368828d3899
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2023-06-12 17:43:33 -0500

    add design of footer

--- app/src/layouts/BasicLayout/BasicLayout.js
+++ app/src/layouts/BasicLayout/BasicLayout.js
@@ -2,22 +2,24 @@ import React from "react";
 import styles from "./BasicLayout.module.scss";
 import { Container } from "semantic-ui-react";
 import classNames from "classnames";
-import { TopBar } from "@/components/layout";
+import { TopBar, Footer } from "@/components/layout";
 
 export function BasicLayout({
   children,
-  isOpenSearch= false,
+  isOpenSearch = false,
   isContainer = false,
   relative = false,
 }) {
   return (
     <>
-      <TopBar isOpenSearch={isOpenSearch}/>
+      <TopBar isOpenSearch={isOpenSearch} />
       <Container fluid>
         <div className={classNames({ [styles.relative]: relative })}>
           {isContainer ? <Container>{children}</Container> : children}
         </div>
       </Container>
+
+      <Footer />
     </>
   );
 }
