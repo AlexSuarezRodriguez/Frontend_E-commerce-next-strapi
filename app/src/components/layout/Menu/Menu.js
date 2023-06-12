119c7fb2987f24cecda850f1273e990deb28746b
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2023-06-12 17:17:05 -0500

    create section menu

--- app/src/components/layout/Menu/Menu.js
+++ app/src/components/layout/Menu/Menu.js
@@ -1,22 +1,75 @@
 import { useState, useEffect } from "react";
-import styles from "./Menu.module.scss";
 import { Image, Icon, Input } from "semantic-ui-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { map } from "lodash";
+import classNames from "classnames";
 import { platform } from "@/api";
+import styles from "./Menu.module.scss";
 
 const platformCtrl = new platform();
 
-export function Menu({ isOpenSearch }) {
+export function Menu(props) {
+  const { isOpenSearch } = props;
   const [platforms, setPlatforms] = useState(null);
+  const [showSearch, setShowSearch] = useState(isOpenSearch);
+  const [searchText, setSearchText] = useState("");
+  const router = useRouter();
+
+  const openCloseSearch = () => setShowSearch((prevState) => !prevState);
+
   useEffect(() => {
     (async () => {
       try {
-        const {data} = await platformCtrl.getAll();
-        setPlatforms(data);
+        const response = await platformCtrl.getAll();
+        setPlatforms(response.data);
       } catch (error) {
         console.error(error);
       }
     })();
   }, []);
 
-  return <div>C Menu</div>;
+  useEffect(() => {
+    setSearchText(router.query.s || "");
+  }, []);
+
+  const onSearch = (text) => {
+    setSearchText(text);
+    router.replace(`/search?s=${text}`);
+  };
+
+  return (
+    <div className={styles.platforms}>
+      {map(platforms, (platform) => (
+        <Link key={platform.id} href={`/games/${platform.attributes.slug}`}>
+          <Image src={platform.attributes.icon.data.attributes.url} />
+          {platform.attributes.title}
+        </Link>
+      ))}
+
+      <button className={styles.search} onClick={openCloseSearch}>
+        <Icon name="search" />
+      </button>
+
+      <div
+        className={classNames(styles.inputContainer, {
+          [styles.active]: showSearch,
+        })}
+      >
+        <Input
+          id="search-games"
+          placeholder="Buscador"
+          className={styles.input}
+          focus={true}
+          value={searchText}
+          onChange={(_, data) => onSearch(data.value)}
+        />
+        <Icon
+          name="close"
+          className={styles.closeInput}
+          onClick={openCloseSearch}
+        />
+      </div>
+    </div>
+  );
 }
