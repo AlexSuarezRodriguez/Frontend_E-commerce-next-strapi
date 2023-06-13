f19c1da811ce2470fbfc8ebfb85d1181b9be85d0
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2023-06-12 23:37:21 -0500

    design page acount

---
+++ app/src/pages/account/account.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { Tab } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import { BasicLayout } from "@/layouts";
+import { useAuth } from "@/hooks";
+import { Info } from "@/components/Account";
+import styles from "./account.module.scss";
+
+export default function AccountPage() {
+  const { user, logout } = useAuth();
+  const router = useRouter();
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user]);
+
+  const onReload = () => setReload((prevState) => !prevState);
+
+  const panes = [
+    {
+      menuItem: "Mis pedidos",
+      render: () => <Tab.Pane attached={false}>pedidos</Tab.Pane>,
+    },
+    {
+      menuItem: "Lista de deseos",
+      render: () => <Tab.Pane attached={false}>lista de deseos</Tab.Pane>,
+    },
+    {
+      menuItem: "Direcciones",
+      render: () => <Tab.Pane attached={false}>Direcciones</Tab.Pane>,
+    },
+    {
+      menuItem: { key: 20, icon: "settings", content: "Ajustes" },
+      render: () => <Tab.Pane attached={false} key={99}>Ajustes</Tab.Pane>,
+    },
+    {
+      menuItem: {
+        key: 21,
+        icon: "log out",
+        content: "",
+        onClick: logout,
+      },
+    },
+  ];
+
+  return (
+    <>
+      <BasicLayout isContainer relative>
+        <Info />
+
+        <Tab
+          menu={{ secondary: true, pointing: true }}
+          panes={panes}
+          className={styles.tabs}
+        />
+      </BasicLayout>
+    </>
+  );
+}
